f9ef29d5e9038943155b193b91e3da111bb9f5fb
az <93015718+FoolishBishop@users.noreply.github.com>
2023-11-03 22:57:51 -0300

    Update readRFID.py

--- metodologia/readRFID.py
+++ metodologia/readRFID.py
@@ -1,3 +1,11 @@
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+
 def read_rfid():
-    # ...
-    print("test")
+    reader = SimpleMFRC522()
+    try:
+        card_id, card_text = reader.read()
+        return card_id
+    finally:
+        GPIO.cleanup()
