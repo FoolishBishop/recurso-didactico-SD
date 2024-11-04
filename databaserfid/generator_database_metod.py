4c0300e5379fbdeb693133e0704084b2938cdcc4
az <93015718+FoolishBishop@users.noreply.github.com>
2024-11-03 22:44:40 -0300

    Delete databaserfid directory

--- databaserfid/generator_database_metod.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conexion = sqlite3.connect("metodologia_rfid.db")
-conexion.execute("""create table if not exists RFID (
-                          ID integer primary key AUTOINCREMENT,
-                          hexcode text,
-                          letter text
-                    )""")
-conexion.close()
