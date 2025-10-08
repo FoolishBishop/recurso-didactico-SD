23c3f32ae688080c4ed52bc96198f70a80dc3738
az <93015718+FoolishBishop@users.noreply.github.com>
2025-10-08 16:28:31 -0300

    Update AduinoMetodologia.ino
    FUNCIONA!!!! SOLO QUEDARIA IMAGENES (para eso es necesario que el arduino mande mensajes al pipico + almacenar imagenes)

--- AduinoMetodologia.ino
+++ AduinoMetodologia.ino
@@ -1,111 +1,76 @@
-// NOTAS
+// ==========================
+// Arduino UNO — pantalla UTFT + encoder + I2C esclavo
+// Recibe desde Pico mensajes tipo "UID:F3C45515" y los muestra
+// ==========================
 
-//*Event Handlers (COMUNICACION ARDUINO/PICO)
-// Wire.onReceive(DataReceive); <-- para recibir data
-// Wire.onRequest(DataRequest); <-- para mandar data0
-
-// LIBRERIAS
-//Libreria comunicacion I2C
 #include <Wire.h>
-
-// Libreria encoder
+#include <UTFT.h>
 #include <SPI.h>
 #include <RotaryEncoder.h>
 
-// Libreria RFID
-#include <Adafruit_PN532.h>  // Library for the NFC/RFID module! !! Please load by Arduino IDE !!!
-// https://funduinoshop.com/en/blog/arduino/the-pn532-nfc-rfid-module-near-field-communication
-// SDA (TXD): A4  SCL (RXD): A5
 
-// Libreria pantalla
-#include <UTFT.h>  //http://www.rinkydinkelectronics.com/library.php?id=51
-//#include <UTFT_Geometry.h>
 
-// Variables comunicacion I2C
+// Dirección I2C del Arduino como esclavo
 #define UNO_ADDR 9
-#define RESP_SIZE 15
-// Variable transferencia de datos
-String dataToSend = "";
-String lastReceived = "";
-bool dataUpdated = false;
-String letraRecibida = "";
+#define RESP_SIZE 32
 
-// Configuración de la pantalla ILI9341
-// UTFT myGLCD(ILI9341_16, 38, 39, 40, 41);
-
+// --- Pantalla ILI9341 (ajusta los pines según tu shield)
+UTFT myGLCD(ILI9341_16, 38, 39, 40, 41);
 extern uint8_t BigFont[];
-// Variables de posición para la flecha
-int variables[] = { 20, 40, 60, 80 };
-int currentIndex = 0;  // Índice de la variable actual para la flecha
 
-// RFID
-unsigned long cardid;               // Variable for the read TAG ID
-#define PN532_IRQ (2)               // Define the IRQ port
-#define PN532_RESET (3)             // Define the Reset connector
-unsigned long startTime;            // Variable para almacenar el tiempo de inicio
-uint8_t success;                    // Create variable
-uint8_t uid[] = { 0, 0, 0, 0, 0 };  // Buffer to store the UID
-uint8_t uidLength;                  // Length of the UID (4 or 7 bytes depending on ISO14443A card/chip type)
-
-static unsigned long lastCardId = 0;
-static int lastPos = -1;
-
-
-Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);  // Create instance with I2C protocol
-
-// ROTATORY ENCODER
-#define PIN_IN1 A2  // CLK
-#define PIN_IN2 A3  // DT
-
-// Create instances
+// --- Encoder
+#define PIN_IN1 A2
+#define PIN_IN2 A3
 RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
-
+volatile bool encoderChanged = false;
+int newPos = 0;
 // Pre-calculate limits and threshold for readability
-const byte MAX_POS = 25;
+const byte MAX_POS = 50;
 const byte CHANGE_THRESHOLD = 3;
-byte letra_pos = 0;
-byte letra_pos_raw = 0;
+byte letra = 0;
 
 // Flag for detecting changes in encoder direction
 volatile bool directionChanged = false;
 
 
+
+// Variables de pantalla
+int currentIndex = 0;
+String received_msg = "";
+int variables[] = { 20, 40, 60, 80 };  // posiciones base
+
+// --- Prototipos
+void drawLines();
+void drawArrow(int index);
+void showText(String msg);
+
+// ==========================
+// SETUP
+// ==========================
 void setup() {
   Serial.begin(9600);
-  Serial.println("Iniciando programa...");
 
-  // I2C
+  // --- I2C
   Wire.begin(UNO_ADDR);
-  Wire.onRequest(DataRequest);
   Wire.onReceive(DataReceive);
-  Serial.println(Wire.available());
-  Serial.println("Comunicacion I2C lista");
+  Wire.onRequest(DataRequest);
 
-
-  // RFID
-  nfc.begin();
-  unsigned long versiondata = nfc.getFirmwareVersion();  // Read version number of firmware
-  if (!versiondata) {                                    // If no response is received
-    Serial.print("Can't find board !");                  // Send text "Can't find..." to serial monitor
-    while (1)
-      ;  // so long stop
-  }
-  Serial.println("Sensor RFID listo");
-
-  // Attach interrupt for efficient rotary encoder handling
+  // --- Encoder
   attachInterrupt(digitalPinToInterrupt(PIN_IN1), encoderChange, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PIN_IN2), encoderChange, CHANGE);
-  Serial.println("Encoder listo");
 
-  // Screen QUITAR DE COMENTARIOS DPS CUANDO ESTE EN EL MEGA !!!!!!!!
-  // myGLCD.InitLCD();
-  // myGLCD.clrScr();
-  // drawLines();
-  // Serial.println("Pantalla lista");
+  // --- Pantalla
+  myGLCD.InitLCD();
+  myGLCD.clrScr();
+  myGLCD.setFont(BigFont);
+  drawLines();
 
-  Serial.println("Arduino UNO listo!");
+  Serial.println("Arduino UNO listo para recibir UID por I2C");
 }
 
+// ==========================
+// LOOP
+// ==========================
 void loop() {
   // Encoder
   static int pos = 0;
@@ -130,139 +95,99 @@ void loop() {
     }
 
     if (pasosDesdeUltimoCambio == CHANGE_THRESHOLD) {
-      letra_pos_raw += direccion;
-      letra_pos = (letra_pos_raw + 4) % 4;  // Limitamos letra entre 0 y 3
+      letra += direccion;
+      letra = (letra + 4) % 4;  // Limitamos letra entre 0 y 3
       pasosDesdeUltimoCambio = 0;
-      Serial.println("Pos: " + String(letra_pos));
-      // envia a pantalla !!!!!!!!!
-      //drawArrow(letra_pos);
+      Serial.print("POS LETRA: ");
+      Serial.println(letra);
     }
     pos = newPos;
-  }
-
-  // RFID
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 30);
-
-  if (success) {
-    if (uidLength == 4) {  // If the card/chip has 4 byte length....
-      // Mifare Classic card
-      cardid = uid[0];   //
-      cardid <<= 8;      // Set the 4 byte blocks
-      cardid |= uid[1];  //
-      cardid <<= 8;      // to a single block
-      cardid |= uid[2];  //
-      cardid <<= 8;      // together
-      cardid |= uid[3];  //
-
-      if (cardid != lastCardId || letra_pos != lastPos) {
-        lastCardId = cardid;
-        lastPos = letra_pos;
-
-        // enviar
-        dataToSend = String(cardid) + "," + String(letra_pos);
-        dataUpdated = true;
-        Serial.println("Actualizado: " + dataToSend);
-        }
-DataRequest() ;
-      }
-    }
-  
-  // recibir mensaje pipico
-  if (letraRecibida != "") {
-    // Hacer algo con la letra, por ejemplo mostrarla en pantalla
-    Serial.println("RECIBIDO MENSAJE PIPICO: " + letraRecibida);
-    
-    // usar pantalla
-    // showLetter(letra_pos, data);
-
-    letraRecibida = "";  // limpiar para que no se repita
-  }
-
-DataRequest();
-  success = 0;
-}
-
-// !!! FUNCIONES !!!
-
-// Funciones de comunicacion I2C
-void DataReceive(int numBytes) {  // recibir data
-  lastReceived = "";
-
-  while (Wire.available()) {
-    char c = Wire.read();
-    lastReceived += c;
-  }
-
-  Serial.println("Raspberry dijo: " + lastReceived);
-}
-
-void DataRequest() {  // mandar data
-  if (dataUpdated) {
-    Wire.write(dataToSend.c_str());
-    dataUpdated = false;  // Evita repetir el mismo mensaje
-    Serial.print("enviado");
-  } else {
-    Wire.write("");  // No hay datos nuevos
+    Serial.println(pos);
+    drawArrow(letra);
   }
 }
 
-// Funciones de pantalla
 
-// void drawLines() {
-//   myGLCD.clrScr();  // Limpia la pantalla
-
-//   int screenWidth = myGLCD.getDisplayXSize();   // Ancho de la pantalla
-//   int screenHeight = myGLCD.getDisplayYSize();  // Alto de la pantalla
-
-//   int lineLength = 50;  // Longitud de las líneas
-//   int spaceX = 25;      // Espaciado entre líneas
-
-//   int startX = (screenWidth - (lineLength * 4 + spaceX * 3)) / 2;
-//   int startY = screenHeight / 2;  // Centrar verticalmente
-
-//   int colors[][3] = { { 255, 255, 255 }, { 255, 255, 255 }, { 255, 255, 255 }, { 255, 255, 255 } };  // Rojo, Verde, Azul, Amarillo
-
-//   for (int i = 0; i < 4; i++) {
-//     myGLCD.setColor(colors[i][0], colors[i][1], colors[i][2]);
-
-//     int x1 = startX + i * (lineLength + spaceX);
-//     int x2 = x1 + lineLength;
-//     myGLCD.drawLine(x1, startY, x2, startY);
-
-//     //myGLCD.setColor(255, 255, 255); // Blanco para el texto
-//     //myGLCD.printNumI(variables[i], x1 + lineLength / 2 - 5, startY - 20);
-//   }
-
-//   // Dibujar la flecha centrada en la línea actual
-//   //int arrowX = startX + currentIndex * (lineLength + spaceX) + lineLength / 2;
-//   //drawArrow(arrowX, startY - 10);
-// }
-
-// void drawArrow(int index) {
-//   //drawArrow(48+74*index, 110);
-//   int x = 48 + 74 * index;
-//   int y = 110;
-//   myGLCD.setColor(0, 0, 0);           // bloque negro
-//   myGLCD.fillRect(0, 125, 300, 140);  // Draw a filled rectangle
-
-//   int arrowSize = 5;
-//   myGLCD.setColor(255, 255, 255);  // Flecha blanca
-//   myGLCD.drawLine(x, y + 20, x - arrowSize, y + arrowSize + 20);
-//   myGLCD.drawLine(x, y + 20, x + arrowSize, y + arrowSize + 20);
-// }
-
-// void showLetter(int index, String letter) {
-//   myGLCD.setColor(0, 0, 0);                                    // bloque negro
-//   myGLCD.fillRect(20 + 74 * index, 90, 70 + 74 * index, 119);  // Draw a filled rectangle
-
-
-//   myGLCD.setColor(255, 255, 255);  // Color blanco para la letra
-//   myGLCD.setFont(BigFont);
-//   myGLCD.print(String(letter), 40 + 74 * index, 100);
-// }
-
-// Funciones de encoder
-// Function to handle encoder state changes (interrupt-safe)
+// ==========================
+// INTERRUPCIONES
+// ==========================
 void encoderChange() {
-  directionChanged = true;
+  encoderChanged = true;
+}
+
+// ==========================
+// FUNCIONES I2C
+// ==========================
+void DataReceive(int numBytes) {
+  char buffer[RESP_SIZE] = { 0 };
+  int i = 0;
+  while (Wire.available() && i < RESP_SIZE - 1) {
+    buffer[i++] = Wire.read();
+  }
+  String msg = String(buffer);
+  msg.trim();
+  Serial.print("I2C Recibido: ");
+  Serial.println(msg);
+
+  if (msg.startsWith("UID:")) {
+    received_msg = msg.substring(4);
+    showLetter(letra, received_msg);
+  }
+}
+
+void DataRequest() {
+  Wire.write("OK\n");
+}
+
+// ==========================
+// FUNCIONES DE PANTALLA
+// ==========================
+void drawLines() {
+  myGLCD.clrScr();
+  int screenWidth = myGLCD.getDisplayXSize();
+  int screenHeight = myGLCD.getDisplayYSize();
+
+  int lineLength = 50;
+  int spaceX = 25;
+  int startX = (screenWidth - (lineLength * 4 + spaceX * 3)) / 2;
+  int startY = screenHeight / 2;
+
+  for (int i = 0; i < 4; i++) {
+    myGLCD.setColor(255, 255, 255);
+    int x1 = startX + i * (lineLength + spaceX);
+    int x2 = x1 + lineLength;
+    myGLCD.drawLine(x1, startY, x2, startY);
+  }
+  drawArrow(currentIndex);
+}
+
+void drawArrow(int index) {
+  myGLCD.setColor(0, 0, 0);
+  myGLCD.fillRect(0, 125, 300, 140);  // limpia
+  int x = 48 + 74 * index;
+  int y = 110;
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.drawLine(x, y + 20, x - 5, y + 25);
+  myGLCD.drawLine(x, y + 20, x + 5, y + 25);
+}
+
+// Mostrar las letras en base a indice y letra
+void showLetter(int index, String letter) {
+  myGLCD.setColor(0, 0, 0);                                    // bloque negro
+  myGLCD.fillRect(20 + 74 * index, 90, 70 + 74 * index, 119);  // Draw a filled rectangle
+
+
+  myGLCD.setColor(255, 255, 255);  // Color blanco para la letra
+  myGLCD.setFont(BigFont);
+  myGLCD.print(String(letter), 40 + 74 * index, 100);
+}
+
+
+
+// este es mas showcase para ver que recibe
+void showText(String msg) {
+  myGLCD.setColor(0, 0, 0);
+  myGLCD.fillRect(0, 60, 320, 100);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.print(msg, CENTER, 80);
 }
